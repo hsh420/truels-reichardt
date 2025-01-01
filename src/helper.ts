504be8bd8365e8fcaff1fa4f713833079467d9ef
Hendrik Schwind-Hansen <info@hendrik-sh.de>
2025-01-01 17:56:11 +0100

    last steps to reach final stage soon yay

---
+++ src/helper.ts
@@ -0,0 +1,32 @@
+interface UmlautMap {
+  [index: string]: string;
+  Ü: string;
+  Ä: string;
+  Ö: string;
+  ü: string;
+  ä: string;
+  ö: string;
+  ß: string;
+}
+
+const umlautMap: UmlautMap = {
+  "\u00dc": "UE",
+  "\u00c4": "AE",
+  "\u00d6": "OE",
+  "\u00fc": "ue",
+  "\u00e4": "ae",
+  "\u00f6": "oe",
+  "\u00df": "ss",
+};
+
+export function replaceUmlaute(str: string): string {
+  return str
+    .replace(/[\u00dc|\u00c4|\u00d6][a-z]/g, (a) => {
+      const big = umlautMap[a.slice(0, 1)];
+      return big.charAt(0) + big.charAt(1).toLowerCase() + a.slice(1);
+    })
+    .replace(
+      new RegExp("[" + Object.keys(umlautMap).join("|") + "]", "g"),
+      (a) => umlautMap[a]
+    );
+}
